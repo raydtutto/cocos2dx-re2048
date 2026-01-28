a3c69f4e7505aa3aaf4cd7911d7468ef435fbc50
raydtutto <raydtutto24@gmail.com>
2026-01-28 19:25:13 +0200

    update GameplayScene.cpp

--- Classes/GameplayScene.cpp
+++ Classes/GameplayScene.cpp
@@ -3,8 +3,7 @@
 USING_NS_CC;
 
 
-bool GameplayScene::init()
-{
+bool GameplayScene::init() {
     if ( !Scene::init() )
     {
         return false;
@@ -12,9 +11,16 @@ bool GameplayScene::init()
 
     auto visibleSize = Director::getInstance()->getVisibleSize();
 
+    // Background color
+    const Color4B colorBg = Color4B(40, 40, 45, 255);
+    const auto layerColor = LayerColor::create(colorBg);
+    addChild(layerColor);
+
     // Board background
     auto boardBg = Sprite::create("GUI/cocosstudio/img/boardBg.png");
+    auto boardSize = boardBg->getContentSize();
     boardBg->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+    boardBg->setScale(std::min(visibleSize.width / boardSize.width, visibleSize.height / boardSize.height));
     addChild(boardBg);
 
     return true;
